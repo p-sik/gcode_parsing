633ec951a57952be0560b0adca07fe456141d6ac
Stacionarni-PC <p3ter.sik@gmail.com>
2018-08-08 19:59:10 +0200

    created some data objects

---
+++ GCodeParser/Models/GCodeLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCodeParser.Models
+{
+    /// <summary>
+    /// Single line of formatted Gcode
+    /// </summary>
+    public class GCodeLine
+    {
+        public float XCoordinate { get; set; }
+        public float YCoordinate { get; set; }
+        public int Layer { get; set; }
+    }
+}
---
+++ GCodeParser/Models/ParsedCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCodeParser.Models
+{
+    /// <summary>
+    /// Object containing all parsed and formatted data
+    /// </summary>
+    public class ParsedCode
+    {
+        //TODO [Serializable] in the future?
+        public int NumberOfLines { get; set; }
+        public List<float> LayerHeights { get; set; }
+        public List<GCodeLine> ParsedLines { get; set; }
+    }
+}
