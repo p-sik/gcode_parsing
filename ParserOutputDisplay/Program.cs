678035149c5ab006d5ee47edd01ce702a62ffe8a
Stacionarni-PC <p3ter.sik@gmail.com>
2018-08-08 19:41:46 +0200

    created console program to test outputs

---
+++ ParserOutputDisplay/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserOutputDisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("G-code parser: Work in progress!");
+            Console.WriteLine("--------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
