e076cd63b7655bdeae16e5e25469e3a478f2b828
Stacionarni-PC <p3ter.sik@gmail.com>
2018-08-10 16:46:49 +0200

    created default and implementable custom filter

--- GCodeParser/Models/ParsingFilter.cs
+++ GCodeParser/Models/ParsingFilter.cs
@@ -6,6 +6,25 @@ namespace GCodeParser.Models
 {
     public class ParsingFilter
     {
+        static string[] defaultFilter = {
+            "G28", // endstop movement
+            "G92", // extruder position set/reset
+            "G90", // absolute positioning command
+            "\n", // empty lines
+            ";" // only comment lines
+        };
 
+        private string[] filter;
+
+        public string[] Filter
+        {
+            get => filter ?? defaultFilter;
+            set => filter = value;
+        }
+
+        public ParsingFilter(params string[] filteredCommands)
+        {
+            filter = new string[filteredCommands.Length];
+        }
     }
 }
