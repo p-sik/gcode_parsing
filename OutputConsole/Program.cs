d94c37d2d385b2127f99fe4e1e7bc7996a0a9725
Stacionarni-PC <p3ter.sik@gmail.com>
2018-08-11 14:34:50 +0200

    changed console app to .NET Core so it's cross-platform

---
+++ OutputConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GCodeParser;
+
+namespace OutputConsole
+{
+    class Program
+    {
+        private const string testPath = @"C:\Users\Peter\Documents\CodeProjects\GCodeParsing\kalib_cil.gcode";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("G-code parser: Work in progress!");
+            Console.WriteLine("--------------------------------");
+
+            List<string> allLines = OutputUtilities.ReadFile(testPath);
+
+            CodeParser parser = new CodeParser(allLines);
+
+            Console.ReadKey();
+        }
+    }
+}
